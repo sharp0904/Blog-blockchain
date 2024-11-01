1cea0d683100cee41d25092fa8fccf8280a026e9
sharp0904 <0xcrazy0904@gmail.com>
2024-11-01 06:20:38 +0100

    firt commit for blog-blockchain

---
+++ x/blog/keeper/msg_server_create_post.go
@@ -0,0 +1,23 @@
+package keeper
+
+import (
+	"context"
+
+	"blog/x/blog/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	var message = types.Post{
+		Creator: msg.Creator,
+		Title: msg.Title,
+		Body: msg.Body,message
+	}
+	
+	id := k.AppendPost(ctx, post)
+
+	return &types.MsgCreatePostResponse{Id: id}, nil
+}
